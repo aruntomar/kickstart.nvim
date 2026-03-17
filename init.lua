792ba8e1ec208ee50d0a1b43278d972223aa1c4e
Arun Tomar <tomar.arun@gmail.com>
2026-03-17 16:47:59 -0400

    add more parsers to treesitter

--- init.lua
+++ init.lua
@@ -875,7 +875,27 @@ require('lazy').setup({
     branch = 'main',
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter-intro`
     config = function()
-      local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
+      local parsers = {
+        'bash',
+        'diff',
+        'go',
+        'gotmpl',
+        'hcl',
+        'html',
+        'json',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'python',
+        'query',
+        'rust',
+        'terraform',
+        'toml',
+        'vim',
+        'vimdoc',
+        'yaml',
+      }
       require('nvim-treesitter').install(parsers)
       vim.api.nvim_create_autocmd('FileType', {
         callback = function(args)
