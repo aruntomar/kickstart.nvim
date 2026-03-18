399e9bb4f1e199f9fe7f9fbe6f9a11db0b2b38ae
Arun Tomar <tomar.arun@gmail.com>
2026-03-18 09:55:19 -0400

    add neo-tree file browser

--- init.lua
+++ init.lua
@@ -204,6 +204,11 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 -- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 -- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
+-- Split windows vertically
+vim.keymap.set('n', '<leader>|', '<C-w>v', { desc = 'Split window vertically' })
+
+-- Open neo-tree file explorer
+vim.keymap.set('n', '<leader>e', ':Neotree filesystem reveal left <CR>', { desc = 'Open neo-tree file explorer' })
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --
